c36681fc0d8a38ef9d44d39413425ddbc234524f
Christelle Van Sebroeck <christelle.marie@gmail.com>
2022-09-04 13:14:29 +0100

    feat: Create controlled form for adding new poll

--- src/components/NewPoll.js
+++ src/components/NewPoll.js
@@ -1,5 +1,54 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const NewPoll = () => {
-  return <div>This is a New Poll</div>;
+  const navigate = useNavigate();
+  const [optionOne, setOptionOne] = useState("");
+  const [optionTwo, setOptionTwo] = useState("");
+
+  const onChangeOptionOne = (e) => {
+    console.log(e.target.value); // TODO - remove
+    setOptionOne(e.target.value);
+  };
+
+  const onChangeOptionTwo = (e) => {
+    console.log(e.target.value); // TODO - remove
+    setOptionTwo(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setOptionOne("");
+    setOptionTwo("");
+    navigate("/");
+  };
+
+  return (
+    <div>
+      <h3>Would You Rather?</h3>
+      <h6>Create Your Own Poll</h6>
+
+      <form className="new-poll" onSubmit={handleSubmit}>
+        <div>
+          <label>First Option</label>
+          <input type="text" value={optionOne} onChange={onChangeOptionOne} />
+        </div>
+        <div>
+          <label>Second Option</label>
+          <input type="text" value={optionTwo} onChange={onChangeOptionTwo} />
+        </div>
+        <div>
+          <button
+            className="btn"
+            type="submit"
+            disabled={optionOne === "" || optionTwo === ""}
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default NewPoll;
