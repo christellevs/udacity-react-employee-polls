c1c5f1257fb164a0fcf936d4b0dd0a350e7de04d
Christelle Van Sebroeck <christelle.marie@gmail.com>
2022-09-08 12:14:00 +0100

    feat: Initialize existing data into App component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import "./App.css";
+import { useEffect } from "react";
+import { connect } from "react-redux";
 import { Route, Routes } from "react-router-dom";
+import { handleInitialData } from "./actions/shared.js";
 import Navbar from "./components/Navbar.js";
 import Dashboard from "./components/Dashboard.js";
 import Leaderboard from "./components/Leaderboard.js";
 import NewPoll from "./components/NewPoll.js";
 import Login from "./components/Login.js";
 
-function App() {
+function App(props) {
+  useEffect(() => {
+    props.dispatch(handleInitialData());
+  }, []);
+
   return (
     <div className="App">
       <Navbar />
@@ -20,4 +27,4 @@ function App() {
   );
 }
 
-export default App;
+export default connect()(App);
