8b79f81ae3de64d40788c1b00925ccd585e5572b
Christelle Van Sebroeck <christelle.marie@gmail.com>
2022-09-08 18:32:41 +0100

    feat: add constrolled component to toggle between answered and unanswered polls

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,20 +1,50 @@
+import { useState } from "react";
 import { connect } from "react-redux";
 import Poll from "./Poll.js";
 
 const Dashboard = (props) => {
+  const [answered, setAnswered] = useState(false);
+  const [unanswered, setUnanswered] = useState(true);
+
+  const onChangeAnswered = (e) => {
+    console.log("answered", e);
+
+    setAnswered(!answered);
+    setUnanswered(!unanswered);
+  };
+
+  const onChangeUnanswered = (e) => {
+    console.log("unanswered", e);
+    setAnswered(!answered);
+    setUnanswered(!unanswered);
+  };
+
   return (
     <div>
-      <h3>Employee Web Polls</h3>
+      <div>
+        <h3>Employee Web Polls</h3>
+        <label>Answered</label>
+        <input
+          type="radio"
+          checked={answered}
+          value={answered}
+          onChange={onChangeAnswered}
+        />
+        <label>Unanswered</label>
+        <input
+          type="radio"
+          checked={unanswered}
+          value={unanswered}
+          onChange={onChangeUnanswered}
+        />
+      </div>
+
       <div>
         <ul>
           {props.questionIds.map((id) => (
             <Poll key={id} id={id} />
           ))}
         </ul>
-        {/* <label>Answered</label>
-        <input type="radio" />Po
-        <label>Unanswered</label>
-        <input type="radio" /> */}
       </div>
     </div>
   );
