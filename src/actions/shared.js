00915355c32365c43d8b3940ae10bd287b174a13
Christelle Van Sebroeck <christelle.marie@gmail.com>
2022-09-08 10:51:20 +0100

    feat: Add actions for shared users and questions

---
+++ src/actions/shared.js
@@ -0,0 +1,12 @@
+import { getInitialData } from "../utils/api.js";
+import { receiveUsers } from "./users.js";
+import { receiveQuestions } from "./questions.js";
+
+export function handleInitialData() {
+  return (dispatch) => {
+    return getInitialData().then(({ users, questions }) => {
+      dispatch(receiveUsers(users));
+      dispatch(receiveQuestions(questions));
+    });
+  };
+}
