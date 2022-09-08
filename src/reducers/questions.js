4cbedc95a9036a3ef626fa0db08e90d8b18c4daa
Christelle Van Sebroeck <christelle.marie@gmail.com>
2022-09-08 11:58:20 +0100

    fix: Module imports for reducers

--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -1,4 +1,4 @@
-import { RECEIVE_QUESTIONS } from "./actions/questions";
+import { RECEIVE_QUESTIONS } from "../actions/questions.js";
 
 export default function users(state = {}, action) {
   switch (action.type) {
