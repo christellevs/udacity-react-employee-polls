b3a5434a4db4e436497ff737f317569b0175bff7
Christelle Van Sebroeck <christelle.marie@gmail.com>
2022-09-08 11:00:33 +0100

    feat: add initial hardcoded authedUser action - need to add authentication later

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,12 +1,16 @@
 import { getInitialData } from "../utils/api.js";
 import { receiveUsers } from "./users.js";
 import { receiveQuestions } from "./questions.js";
+import { setAuthedUser } from "./authedUser";
+
+const AUTHED_ID = "sarahedo";
 
 export function handleInitialData() {
   return (dispatch) => {
     return getInitialData().then(({ users, questions }) => {
       dispatch(receiveUsers(users));
       dispatch(receiveQuestions(questions));
+      dispatch(setAuthedUser(AUTHED_ID));
     });
   };
 }
