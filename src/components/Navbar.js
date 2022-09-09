54d5ea560c534fc019116ee931356bf146605aed
Christelle Van Sebroeck <christelle.marie@gmail.com>
2022-09-09 15:49:33 +0100

    feat: Show name of logged in user in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,13 @@
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-const Navbar = () => {
+
+const Navbar = (props) => {
   return (
     <nav>
       <Link to="/">Home</Link>
       <Link to="/add">New Poll</Link>
       <Link to="/leaderboard">Leaderboard</Link>
-      <span>Username</span>
+      <span>{props.user.name}</span>
       <Link to="/login">
         <button>Logout</button>
       </Link>
@@ -13,4 +15,11 @@ const Navbar = () => {
   );
 };
 
-export default Navbar;
+const mapStateToProps = ({ authedUser, users }) => {
+  const user = users[authedUser];
+  return {
+    user,
+  };
+};
+
+export default connect(mapStateToProps)(Navbar);
