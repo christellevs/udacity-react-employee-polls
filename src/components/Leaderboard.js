2d6ce614daa068875e8738b2055e731aca347e60
Christelle Van Sebroeck <christelle.marie@gmail.com>
2022-09-12 11:08:28 +0100

    feat: Leaderboard component created

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,5 +1,41 @@
-const Leaderboard = () => {
-  return <div>This is a Leaderboard</div>;
+import { connect } from "react-redux";
+
+const Leaderboard = (props) => {
+  console.log("leaderbaord", props);
+  return (
+    <div>
+      <h3>Leaderboard</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>User</th>
+            <th>Answered</th>
+            <th>Created</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.users.map((user) => (
+            <tr key={user.id}>
+              <td>
+                {user.name} <img src={user.avatarURL} alt="User Avatar URL" />
+              </td>
+              <td>{Object.keys(user.answers).length}</td>
+              <td>{user.questions.length}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
-export default Leaderboard;
+const mapStateToProps = ({ users }) => ({
+  users: Object.values(users).sort(
+    (a, b) =>
+      Object.keys(b.answers).length +
+      b.questions.length -
+      (Object.keys(a.answers).length + a.questions.length)
+  ),
+});
+
+export default connect(mapStateToProps)(Leaderboard);
