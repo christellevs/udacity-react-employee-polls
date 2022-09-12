a80ed7ac545f76d7886dad82a15eaded7a06f41a
Christelle Van Sebroeck <christelle.marie@gmail.com>
2022-09-12 19:29:54 +0100

    style: improve navbar style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,13 +3,22 @@ import { Link } from "react-router-dom";
 
 const Navbar = (props) => {
   return (
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/add">New Poll</Link>
-      <Link to="/leaderboard">Leaderboard</Link>
-      <span>{props.user.name}</span>
-      <Link to="/login">
-        <button>Logout</button>
+    <nav className="navbar">
+      <Link to="/" className="navbar-element">
+        Home
+      </Link>
+      <Link to="/add" className="navbar-element">
+        New Poll
+      </Link>
+      <Link to="/leaderboard" className="navbar-element">
+        Leaderboard
+      </Link>
+      <span className="navbar-element navbar-user">
+        <img className="navbar-user-img" src={props.user.avatarURL} />{" "}
+        {props.user.name}
+      </span>
+      <Link to="/login" className="navbar-element">
+        <button className="logout-button">Logout</button>
       </Link>
     </nav>
   );
