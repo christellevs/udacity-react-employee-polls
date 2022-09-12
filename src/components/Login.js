65b09135dce17e33c431c06dd52828595f0a8f7c
Christelle Van Sebroeck <christelle.marie@gmail.com>
2022-09-12 15:35:22 +0100

    feat: AuthedUser being saved to backend

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,39 @@
-const Login = () => {
-  return <div>This is a Login page</div>;
+import { useState } from "react";
+import { connect } from "react-redux";
+import { setAuthedUser } from "../actions/authedUser.js";
+
+const Login = (props) => {
+  const [selectedUser, setSelectedUser] = useState(props.users[0].id);
+
+  const onChange = (e) => {
+    setSelectedUser(e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    props.dispatch(setAuthedUser(selectedUser));
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <select value={selectedUser} onChange={onChange}>
+          {props.users.map((user) => (
+            <option key={user.id} value={user.id}>
+              {user.name}
+            </option>
+          ))}
+        </select>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 };
 
-export default Login;
+const mapStateToProps = ({ users }) => {
+  return {
+    users: Object.values(users),
+  };
+};
+
+export default connect(mapStateToProps)(Login);
--- src/reducers/authedUser.js
+++ src/reducers/authedUser.js
@@ -1,9 +1,11 @@
-import { SET_AUTHED_USER } from "../actions/authedUser.js";
+import { SET_AUTHED_USER, LOGOUT_AUTHED_USER } from "../actions/authedUser.js";
 
 export default function authedUser(state = null, action) {
   switch (action.type) {
     case SET_AUTHED_USER:
       return action.id;
+    case LOGOUT_AUTHED_USER:
+      return null;
     default:
       return state;
   }
