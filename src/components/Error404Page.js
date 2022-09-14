01627088af2a5ac3ade30786fc8228800523e5e0
Christelle Van Sebroeck <christelle.marie@gmail.com>
2022-09-14 16:03:07 +0100

    refactor: simplify 404 page

--- src/components/Error404Page.js
+++ src/components/Error404Page.js
@@ -1,19 +1,8 @@
-import { useNavigate } from "react-router-dom";
-
 const Error404Page = () => {
-  const navigate = useNavigate();
-
-  const onClick = (e) => {
-    e.preventDefault();
-    navigate("/login");
-  };
   return (
     <div>
       <h1>Error 404</h1>
       <h2> Page not Found!</h2>
-      <button onClick={onClick} className="btn btn-login">
-        Back to Login
-      </button>
     </div>
   );
 };
